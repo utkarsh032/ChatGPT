8ed3fc2c0344b39b607673dcf040feb74874fa12
utkarsh032 <utkarshraj525@gmail.com>
2024-11-18 10:48:30 +0530

    Send Promot as a Outgoing Msg

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-const sidebar = document.querySelector('.sidebar');
-const closeBtn = document.querySelector('.close-btn');
+const chatInput = document.querySelector('#chat-input');
+const sendButton = document.querySelector('#send-btn');
+const chatContainer = document.querySelector('.chat-container');
 
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('open');
-});
+let userText = null;
+
+const createElement = (html, className) => {
+  const chatDiv = document.createElement('div');
+  chatDiv.classList.add('chat', className);
+  chatDiv.innerHTML = html;
+  return chatDiv;
+};
+
+const handleOutGoingChat = () => {
+  userText = chatInput.value.trim();
+  const html = `
+    <div class="chat-content">
+      <div class="chat-details">
+        <span class="material-symbols-outlined">
+          edit
+        </span>
+        <p>${userText}</p>
+        <img src="./img/user.jpeg" alt="user">
+      </div>
+    </div>
+  `;
+
+  const outGoingChatDiv = createElement(html, 'outgoing');
+  chatContainer.appendChild(outGoingChatDiv);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  chatInput.value = '';
+};
+
+sendButton.addEventListener('click', handleOutGoingChat);
